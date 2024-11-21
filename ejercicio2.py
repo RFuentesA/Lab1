336a7f3e3b0afedd4ddd4da300f0b8a73825e1f2
SebastiánMartínez <159493363+S3bastian2@users.noreply.github.com>
2024-11-21 10:25:43 -0500

    Correciones en el punto 2

--- ejercicio2.py
+++ ejercicio2.py
@@ -4,6 +4,6 @@ with open("test_pr2.txt", "r") as archivo:
     contenido = archivo.read()
 texto_leido = contenido.split()
 texto_convertido = [palabra.lower() for palabra in texto_leido]
-print(texto_convertido)
+#print(texto_convertido)
 ocurrencia = 'en'
 print(texto_convertido.count(ocurrencia))
