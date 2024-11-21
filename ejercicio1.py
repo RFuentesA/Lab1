1f648e94b8b6dc41fedcdd3cb287f859d0753334
SebastiánMartínez <159493363+S3bastian2@users.noreply.github.com>
2024-11-21 11:29:38 -0500

    Primera parte ejercicio 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,21 +1,32 @@
 #punto 1
-n = int(input())
+n = int(input("Ingrese la cantidad maxima de valores que quiera: "))
 valores = []
 for i in range(n):
-  valores.append(int(input()))
+  valores.append(int(input("Ingresa un numero: ")))
 
 
 max = 0
-for i in range(len(valores)):
+min = 0
+for i in range(n):
   if valores[i] > max:
     max = valores[i]
-print("El valor maximo es: ", str(max))
+    
+x = max
+for j in range(len(valores)):
+  if valores[j] < x:
+    min = valores[j]
+    x = valores[j]
+  else:
+    min = x
 
-min = 0
-for i in range(len(valores)):
-  if valores[i] < min:
-    min = valores[i]
-print("El valor maximo es: ", str(min))
+
+print("El valor maximo es: ", str(max))
+print("El valor minimo es: ", str(min))
+
+
+
+
+
 
 
 
