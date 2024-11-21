2371a9afb1be5ae1c3283ef605aafd6065a44a71
SebastiánMartínez <159493363+S3bastian2@users.noreply.github.com>
2024-11-21 10:20:16 -0500

    metiendo ejercicio 3

---
+++ ejercicio3.py
@@ -0,0 +1,21 @@
+#punto 3
+
+listaUsuarios = {"Juan1223": "J12an*.",
+                 "Maria2345":"M23a*.",
+                 "Pablo1459":"P14o*.",
+                 "Ana3456":"A34a*."}
+intentosFallidos = 0
+
+#Logica de acceso
+for fallido in range(3):
+    usuario = input("Ingrese su usuario ")
+    contraseña = input("Ingrese su contraseña ")
+    if usuario not in listaUsuarios and contraseña not in listaUsuarios:
+        intentosFallidos += 1
+        if intentosFallidos == 3:
+            print("Lo siento, su acceso no es permitido ")
+            break
+        print("Datos incorrectos")
+    else:
+        print("Acceso permitido")
+        break
