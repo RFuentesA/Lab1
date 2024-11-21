483c33a7dcd1d266ad003a77518af5527e9633a4
Richi <rfuentesa@unal.edu.co>
2024-11-21 10:29:55 -0500

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -4,10 +4,26 @@ valores = []
 for i in range(n):
   valores.append(int(input()))
 
-print("El valor maximo es: ", max(valores))
 
-print("El valor mínimo es: ", min(valores))
+max = 0
+for i in range(len(valores)):
+  if valores[i] > max:
+    max = valores[i]
+print("El valor maximo es: ", str(max))
+   
+
+
+
+
+
+
+
+
+
+
+
+"""print("El valor mínimo es: ", min(valores))
 
 print("La suma de lo ingresado es: ", sum(valores))
 
-print("El promedio de lo ingresado es: ", sum(valores)/len(valores))
+print("El promedio de lo ingresado es: ", sum(valores)/len(valores))"""
