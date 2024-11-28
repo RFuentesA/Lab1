ae0f883bb87108456e8e054040c6437da23fbcdb
SebastiánMartínez <159493363+S3bastian2@users.noreply.github.com>
2024-11-28 10:26:43 -0500

    Push

--- ejercicio2.py
+++ ejercicio2.py
@@ -6,4 +6,4 @@ texto_leido = contenido.split()
 texto_convertido = [palabra.lower() for palabra in texto_leido]
 #print(texto_convertido)
 ocurrencia = 'en'
-print(texto_convertido.count(ocurrencia))
+print("El numero de ocurrencias <en> en el texto son: ", texto_convertido.count(ocurrencia))
